767f4a1160269c77e2a0257475eb65a93decfb36
David <none@none>
2017-01-04 22:29:05 -0500

    veracrypt - firstrun script should copy a valid config file into the config volume if one does not already exist.

--- veracrypt/firstrun.sh
+++ veracrypt/firstrun.sh
@@ -5,4 +5,7 @@
   chown -R nobody:users /nobody/.config/VeraCrypt/
   chmod -R g+rw /nobody/.config/VeraCrypt/
 
+  [ -f /nobody/.config/VeraCrypt/Configuration.xml ] || cp /Configuration.xml /nobody/.config/VeraCrypt/Configuration.xml
+  chown -R nobody:users /nobody/.config/VeraCrypt/Configuration.xml
+  
   chown -R nobody:users /mnt/disks
