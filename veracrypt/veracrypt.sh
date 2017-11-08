c6634b2b9676047cb9be2398fdd8d2fa080cf7bf
dcflachs <dcflachs@gmail.com>
2017-11-07 20:34:49 -0500

    Maybe using usermod

--- veracrypt/veracrypt.sh
+++ veracrypt/veracrypt.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/with-contenv sh
 
+#Add the user to the sudo group
+usermod -a -G sudo -u $USER_ID
+
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
@@ -12,9 +15,6 @@ if [ ! -f "$XDG_CONFIG_HOME/VeraCrypt/Configuration.xml" ]
 then
   cp -v /defaults/Configuration.xml "$XDG_CONFIG_HOME/VeraCrypt"
 fi
-
-#Add the user to the sudo group
-adduser $USER_ID sudo
 
 # Adjust ownership of /config.
 chown -R $USER_ID:$GROUP_ID /config
