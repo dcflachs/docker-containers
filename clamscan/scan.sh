bf5ae72f81bcaaac32d42b2f65d21baf30df7512
dcflachs <dcflachs@gmail.com>
2018-03-19 19:22:29 -0400

    Fixing bash if statements, adding priority flag.

--- clamscan/scan.sh
+++ clamscan/scan.sh
@@ -6,13 +6,13 @@ LOGFILE="/var/log/clamav/clamav-$(date +'%Y-%m-%d').log";
 # get the value of "Infected lines"
 MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3);
 
-if [ -z "$PUSHOVER_TOKEN" ] && [ -z "$PUSHOVER_USER" ];then
+if [ ! -z "$PUSHOVER_TOKEN" ] && [ ! -z "$PUSHOVER_USER" ];then
 	# if the value is not equal to zero, send an email with the log file attached
 	if [ "$MALWARE" -ne "0" ];then
-		if [ -z "$INSTANCE_NAME" ];then
-			curl --data "token=$PUSHOVER_TOKEN" --data "user=$PUSHOVER_USER" --data "title=$INSTANCE_NAME - Malware Detected" --data "message=Clamscan detected malware, review logs immediately!" https://api.pushover.net/1/messages.json;
+		if [ ! -z "$INSTANCE_NAME" ];then
+			curl --data "token=$PUSHOVER_TOKEN" --data "user=$PUSHOVER_USER" --data "priority=1" --data "title=$INSTANCE_NAME - Malware Detected" --data "message=Clamscan detected malware, review logs immediately!" https://api.pushover.net/1/messages.json;
 		else
-			curl --data "token=$PUSHOVER_TOKEN" --data "user=$PUSHOVER_USER" --data "title=Malware Detected" --data "message=Clamscan detected malware, review logs immediately!" https://api.pushover.net/1/messages.json;
+			curl --data "token=$PUSHOVER_TOKEN" --data "user=$PUSHOVER_USER" --data "priority=1" --data "title=Malware Detected" --data "message=Clamscan detected malware, review logs immediately!" https://api.pushover.net/1/messages.json;
 		fi
 	fi
 fi 
