028ec7eddde96e2c674f9533c58d6d17b04c19b2
dcflachs <dcflachs@users.noreply.github.com>
2021-10-05 21:34:36 -0400

    add sudoers script

---
+++ veracrypt/sudoers.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv sh
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error.
+
+echo "$APP_USER ALL=(root) NOPASSWD:/usr/bin/veracrypt" > /etc/sudoers.d/veracrypt
+
+chmod 440 /etc/sudoers.d/veracrypt
