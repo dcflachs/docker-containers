15015c09ee8e6a96ab5bb18f6b3485cb65fa3e63
dcflachs <dcflachs@users.noreply.github.com>
2021-10-11 21:08:46 -0400

    test build

--- veracrypt/sudoers.sh
+++ veracrypt/sudoers.sh
@@ -4,5 +4,6 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 echo "$APP_USER ALL=(root) NOPASSWD: /usr/bin/veracrypt, /usr/bin/uptime" > /etc/sudoers.d/veracrypt
+echo "Defaults:$APP_USER !requiretty" > /etc/sudoers.d/veracrypt
 
 chmod 440 /etc/sudoers.d/veracrypt
