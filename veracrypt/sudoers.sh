6a6b421b37d1ac0e52bd0cabb8b51ee93b08f8d6
dcflachs <dcflachs@users.noreply.github.com>
2021-12-27 15:37:51 -0500

    test

--- veracrypt/sudoers.sh
+++ veracrypt/sudoers.sh
@@ -3,9 +3,9 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-echo "$APP_USER ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/veracrypt
+# echo "$APP_USER ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/veracrypt
 
-chmod 440 /etc/sudoers.d/veracrypt
+# chmod 440 /etc/sudoers.d/veracrypt
 
 echo "Defaults logfile=/var/log/sudo" > /etc/sudoers.d/logging
 
