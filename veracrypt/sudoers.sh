c86937661ab53411a20485c9e9d167b7cd0007f7
dcflachs <dcflachs@gmail.com>
2024-09-11 22:31:30 -0400

    Fixes to support updated base image

--- veracrypt/sudoers.sh
+++ veracrypt/sudoers.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/with-contenv sh
+#!/bin/sh
 
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-echo "$APP_USER ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/veracrypt
+echo "app ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/veracrypt
 
 chmod 440 /etc/sudoers.d/veracrypt
 
 # echo "Defaults logfile=/var/log/sudo" > /etc/sudoers.d/logging
 
-# chmod 440 /etc/sudoers.d/logging
+# chmod 440 /etc/sudoers.d/loggingdocke
